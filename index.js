583202c25f5d869dbebd844d5ce98c76ec652f33
Daniel Williams <danielwil621@gmail.com>
2021-05-27 02:51:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.floor(Math.random()*99);
+cart.push(new Object({[item]:price}));
+console.log(`${item} has been added to your cart.`);
+return cart;
 }
 
 function viewCart() {
